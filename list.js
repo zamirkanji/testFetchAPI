3555bab884788a681c7d0853f2f730715f36abb5
Zamir Kanji <zamirkanji@comcast.net>
2022-02-24 12:35:01 -0800

    last thing is the next button

---
+++ list.js
@@ -0,0 +1,14 @@
+const memeWords = [
+    'poo',
+    'dog',
+    'cat',
+    'fall',
+    'kermit',
+    'drake',
+    'phone',
+    'computer',
+    'storm',
+    'lizard',
+    'geiko',
+    'tape'
+]
